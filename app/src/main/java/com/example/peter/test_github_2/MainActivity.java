67cf0624e0c9b4e5efee1a256d554e2c47077ce2
horec <horec25@gmail.com>
2019-11-20 20:49:34 +0100

    commit 128_PC_1

--- app/src/main/java/com/example/peter/test_github_2/MainActivity.java
+++ app/src/main/java/com/example/peter/test_github_2/MainActivity.java
@@ -9,5 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {  //
         super.onCreate(savedInstanceState);   //
         setContentView(R.layout.activity_main);   //  //
-    }   // //zmena 128_PC
+    }   // //zmena 128_PC_1
 }
